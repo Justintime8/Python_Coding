f663b23b439e5c8e8728ca77325b2f3a6f4b38f2
Justin Mendoza <49258832+Justintime8@users.noreply.github.com>
2020-11-21 21:06:49 -0500

    Add files via upload

---
+++ second.py
@@ -0,0 +1,8 @@
+astr = 'hello bob'
+istr = 0
+try:
+    istr = int(astr)
+except:
+    istr = -1
+
+print('done',istr)
---
+++ third.py
@@ -0,0 +1,11 @@
+hrs = input("Enter Hours:")
+h = float(hrs)
+ot = input('enter:')
+o = float(ot)
+rate = input('enter:')
+r = float(rate)
+otrate = input('enter:')
+otr = float(otrate)
+
+if h > 40:
+    print( 40 * r + o *(r * otr))
