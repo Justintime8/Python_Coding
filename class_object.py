11e348e6ba17638938e5de5b757b7deff2a62b01
Justin Mendoza <49258832+Justintime8@users.noreply.github.com>
2021-01-03 20:06:54 -0500

    Add files via upload

---
+++ class_object.py
@@ -0,0 +1,12 @@
+class koreanfriedchickenclubmember:
+    x = 0
+
+    def membership(self) :
+        self.x = self.x + 1
+        print("We have", self.x)
+
+member = koreanfriedchickenclubmember()
+
+member.membership()
+member.membership()
+member.membership()
